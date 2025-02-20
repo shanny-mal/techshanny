fbffcb8c7cc0c36475626dfeb651cbdfd8ce5c0e
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-02-20 23:27:31 +0200

    Correct image path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
-import Services from "./components/Services/Services";
+import Services from "./components/Services";
 import Portfolio from "./components/Portifolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
