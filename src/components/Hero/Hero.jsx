9c06c51a70225c5928ffda9c90f9a00a816ca449
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-02-21 00:38:05 +0200

    Sliding images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+// src/components/Hero.jsx
 import React from "react";
 import { Container, Button } from "react-bootstrap";
 import "./Hero.css";
@@ -5,6 +6,14 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <section id="home" className="hero-section">
+      {/* Background slider */}
+      <div className="hero-slider">
+        <div className="hero-slide"></div>
+        <div className="hero-slide"></div>
+        <div className="hero-slide"></div>
+      </div>
+
+      {/* Content overlay */}
       <Container className="hero-content text-center">
         <h1>Welcome to ShannyTechSolutions</h1>
         <p>Your technology partner for innovation</p>
