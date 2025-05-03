b95a0cac34de046d5bc186298b9c78bfed369dde
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-05-03 20:58:19 +0200

    updated language and tawk

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
 // src/i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./locales/en.json";
 import fr from "./locales/fr.json";
 import es from "./locales/es.json";
