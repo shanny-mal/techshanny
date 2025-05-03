04434eda48992a1c836bfd149305c9605fa6062a
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-05-03 20:25:58 +0200

    updated language detectactor

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,35 @@
 // src/i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./locales/en.json";
 import fr from "./locales/fr.json";
 import es from "./locales/es.json";
 
+// Conditionally load the language detector only in browser
+let LanguageDetector;
+if (typeof window !== "undefined") {
+  try {
+    // eslint-disable-next-line import/no-extraneous-dependencies
+    LanguageDetector = require("i18next-browser-languagedetector").default;
+  } catch (err) {
+    console.warn(
+      "i18next-browser-languagedetector not found, skipping language detection"
+    );
+  }
+}
+
+const detector = LanguageDetector
+  ? LanguageDetector
+  : {
+      type: "languageDetector",
+      detect: () => "en",
+      init: () => {},
+      cacheUserLanguage: () => {},
+    };
+
 i18n
-  // Detect user language
-  .use(LanguageDetector)
+  // Use the detector or fallback stub
+  .use(detector)
   // Pass the i18n instance to react-i18next
   .use(initReactI18next)
   .init({
@@ -20,16 +41,14 @@ i18n
     fallbackLng: "en",
     supportedLngs: ["en", "fr", "es"],
     detection: {
-      // Order and from where user language should be detected
       order: ["querystring", "cookie", "localStorage", "navigator", "htmlTag"],
-      // Cache user language on
       caches: ["cookie", "localStorage"],
     },
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
     react: {
-      useSuspense: false, // true by default - set to false if you don't want to wrap your components in Suspense
+      useSuspense: false, // disable suspense
     },
   });
 
