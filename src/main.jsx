e81ddaffc682eaffa5672cb929beda0f181b56be
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-06-16 13:47:51 +0200

    recreating the website

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
-// src/main.jsx
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
-// ⚙️ Initialize i18next before anything else
-import "./i18n";
-
-import { AuthProvider } from "./context/AuthContext.jsx";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import App from "./App.jsx";
 import "./App.css";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
