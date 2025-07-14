e2553183055c56470fb3f59caf6312ec511f6b49
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-07-14 10:30:04 +0200

    updated contact picture

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -1,7 +1,7 @@
 // src/components/sections/ContactSection.jsx
 import ContactForm from "../forms/ContactForm";
 import { motion } from "framer-motion";
-import contactIllustration from "../../assets/images/contact-illustration.svg";
+import contactIllustration from "../../assets/images/contact-illustration.jpg";
 
 export default function ContactSection() {
   const container = {
